42121dc8eb0e115ba364a5472bd0e858f1b6a785
obbap1 <paschal.obba@stu.cu.edu.ng>
2019-01-10 03:07:28 +0100

    Fix: fix backend url

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ import {InMemoryCache} from 'apollo-cache-inmemory'
 import {CachePersistor} from 'apollo-cache-persist'
 
 const httpLink = new HttpLink({
-    uri:'http://localhost:4000/'
+    uri:'https://medium-followers.herokuapp.com/'
 })
 export const cache = new InMemoryCache()
 
