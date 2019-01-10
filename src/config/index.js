c670a018306b0290545e1feec479a9e7e5834296
obbap1 <paschal.obba@stu.cu.edu.ng>
2019-01-10 03:12:13 +0100

    fix: fix deployment url

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,12 @@ import {HttpLink} from 'apollo-link-http'
 import {InMemoryCache} from 'apollo-cache-inmemory'
 import {CachePersistor} from 'apollo-cache-persist'
 
+let uri;
+
+process.env.NODE_ENV == 'development' ? uri = "https://localhost:4000" : uri = 'https://medium-followers.herokuapp.com/'
+
 const httpLink = new HttpLink({
-    uri:'https://medium-followers.herokuapp.com/'
+    uri:uri
 })
 export const cache = new InMemoryCache()
 
